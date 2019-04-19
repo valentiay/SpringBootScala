a24b34f910d75cd06f0df6a7c4badb168d411cd8
Opolskiy <opolskiy@bpcbt.com>
2019-04-19 11:40:52 +0300

    EchoBot the timer is work

--- src/main/scala/com/ironworkman/telegramm/IronWorkManBot.scala
+++ src/main/scala/com/ironworkman/telegramm/IronWorkManBot.scala
@@ -1,13 +1,35 @@
 package com.ironworkman.telegramm
 
-import info.mukel.telegrambot4s._, api._, methods._, models._, Implicits._
+import info.mukel.telegrambot4s._
+import api._
+import methods._
+import models._
+import Implicits._
+import cats.effect.{ExitCode, IO, Timer}
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
+import scala.language.postfixOps
 
 object IronWorkManBot extends TelegramBot with Polling with Commands {
+
+  implicit val timer = IO.timer(ExecutionContext.global)
   def token = "767996938:AAF6talqUn--PI0z2vJeAxcOtvMRWrQkevw"
+  def printlnIO(str: String): IO[Unit] = IO(println(str))
+
+  def program(msg: String, chatID: Long): IO[Unit] =
+    for {
+      _ <- printlnIO(msg)
+      _ <- IO(request(SendMessage(chatID, msg)))
+      _ <- Timer[IO].sleep(5 second)
+      _ <- IO(request(SendMessage(chatID, msg)))
+    } yield ()
+
 
   override def onMessage(message: Message) = message.text match {
-    case Some(text) => request(SendMessage(message.chat.id, text))
-    case _ => println("It had not a text")
+    case Some(text) => program(text, message.chat.id).unsafeRunSync
+    case _ => {
+      println("No methods")
+    }
   }
 
   def main(args: Array[String]): Unit = {
