ecd6b989209b3a1655132e2e62fd501389a352b8
Opolskiy <opolskiy@bpcbt.com>
2019-04-25 18:07:09 +0300

    Added scheduller in chat.

--- src/main/scala/com/ironworkman/telegramm/IronWorkManBot.scala
+++ src/main/scala/com/ironworkman/telegramm/IronWorkManBot.scala
@@ -3,59 +3,45 @@ package com.ironworkman.telegramm
 import info.mukel.telegrambot4s._
 import api._
 import methods._
-import models._
-import Implicits._
-import cats.effect.{IO, Timer}
+import cats.effect.{ExitCode, IO, Timer}
 
-import scala.io.StdIn
 import scala.language.postfixOps
 import cats.implicits._
+import com.ironworkman.console.ConsoleScheduller.scheduler
+import com.ironworkman.telegramm.IronWorkManBot.reply
+import info.mukel.telegrambot4s.api.declarative.{Action, Commands}
 
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
 import scala.language.postfixOps
 
 object IronWorkManBot extends TelegramBot with Polling with Commands {
-
   implicit val timer = IO.timer(ExecutionContext.global)
+
   def token = "767996938:AAF6talqUn--PI0z2vJeAxcOtvMRWrQkevw"
-  val readLnIO: IO[String] = IO(StdIn.readLine()) //TODO : make async !!!
-  def sendMessageIO(chatID: Long, msg:String): IO[Unit] = IO(request(SendMessage(chatID, msg)))
+
+  def sendMessageIO(chatID: Long, msg: String): IO[Unit] = IO(request(SendMessage(chatID, msg)))
+
   def cleanAndToLong(input: String): Long = input.replace(" ", "").toLong
 
-  def elapsed(count: Long = 0, duration: Long, intervalAmount: Long, chatId: Long, input: String): IO[(Long, Long, Long)] =
-    for {
-      acc <- input match {
-        case _ if count == intervalAmount => sendMessageIO(chatId, "This is a statistic! ") *>
-          IO(0L, 0L, 0L)
-        case _ if  count < intervalAmount => elapsed(count + 1, duration, intervalAmount, chatId, input)
-      }
-      inputs <- IO(input.split(";"))
-      acc <- input.split(";") match {
-        case Array(_,_,_) =>  IO ((acc._1 + cleanAndToLong (inputs (0)),
-          acc._2 + cleanAndToLong (inputs (1)),
-          acc._3 + cleanAndToLong (inputs (2))))
-        case _ => for {
-          _ <- sendMessageIO(chatId, s"The $count interval started. Work!")
-          _ <- Timer[IO].sleep(duration second)
-          _ <- sendMessageIO(chatId, s"The $count interval finished. Whats done?")
-          _ <- sendMessageIO(chatId, s"Print minutes time for 1,2,3 categories separated by ;")
-        } yield acc
-      }
-    } yield acc
+  def printlnIO(str: String) = IO(println(str))
 
-  def scheduler(count:Long, seconds :Long,  chatId: Long): IO[Unit] =
+  def scheduler(count: Long, amount: Long, duration: Long, chatId: Long): IO[Unit] =
     for {
-      _ <- seconds-count match {
-        case 0 => for {
-          _ <- sendMessageIO(chatId, s"First interval is finished after $count seconds")
-        } yield()
-        case _ => for {
-          _ <- Timer[IO].sleep(1 second)
-          _ <- sendMessageIO(chatId, s"next $count seconds")
-          _ <- scheduler(count+1, seconds, chatId)
-        } yield()
-      }
+      _ <- amount - count match {
+            case 0 =>
+              for {
+                _ <- sendMessageIO(chatId, s"Your work is finished after $count intervals")
+              } yield ()
+            case _ =>
+              for {
+                _ <- sendMessageIO(chatId, s"The $count interval started, work!")
+                _ <- Timer[IO].sleep(duration second)
+                _ <- sendMessageIO(chatId, s"Write please whats done during the $count interval")
+                _ <- Timer[IO].sleep(5 second)
+                _ <- scheduler(count + 1, amount, duration, chatId)
+              } yield ()
+          }
     } yield ()
 
   def respond(msg: String, chatID: Long): IO[Unit] =
@@ -63,11 +49,50 @@ object IronWorkManBot extends TelegramBot with Polling with Commands {
       _ <- sendMessageIO(chatID, msg)
     } yield ()
 
-
-  override def onMessage(message: Message) = message.text match {
-    case Some(text) if text == "start"  => respond("Please enter 3 intervals separated by ;", message.chat.id).unsafeRunAsyncAndForget()
-    case Some(text)  =>  scheduler(0, 5, message.chat.id).unsafeRunAsyncAndForget()
+  onCommand("/hello") { implicit msg =>
+    using(_.from) { user =>
+      for {
+        // TODO: Adding user to database
+        _ <- reply(s"Hello ${user.firstName} ${user.lastName}! I`m IronWorkMan bot")
+        _ <- reply(s"Enter please /start with intervals amount and duration")
+        _ <- reply(s"/start 5 30")
+        _ <- reply(s"This started sprint with 5 intervals of 30 minutes duration")
+      } yield ExitCode.Success
+    }
   }
+
+  onCommand("/start") { implicit msg =>
+    withArgs { args =>
+      scheduler(0, args.head.toLong, args(1).toLong, msg.chat.id).unsafeRunAsyncAndForget()
+      // TODO: saving to database
+    }
+  }
+
+  def elapsed(count: Long = 0,
+              intervalDuration: Long,
+              intervalAmount: Long,
+              chatId: Long,
+              input: String): IO[(Long, Long, Long)] =
+    for {
+      acc <- input match {
+              case _ if count == intervalAmount =>
+                sendMessageIO(chatId, "This is a statistic! ") *>
+                  IO(0L, 0L, 0L)
+              case _ if count < intervalAmount => elapsed(count + 1, intervalDuration, intervalAmount, chatId, input)
+            }
+      inputs <- IO(input.split(";"))
+      acc <- input.split(";") match {
+              case Array(_, _, _) =>
+                IO((acc._1 + cleanAndToLong(inputs(0)), acc._2 + cleanAndToLong(inputs(1)), acc._3 + cleanAndToLong(inputs(2))))
+              case _ =>
+                for {
+                  _ <- sendMessageIO(chatId, s"The $count interval started. Work!")
+                  _ <- Timer[IO].sleep(intervalDuration second)
+                  _ <- sendMessageIO(chatId, s"The $count interval finished. Whats done?")
+                  _ <- sendMessageIO(chatId, s"Print minutes time for 1,2,3 categories separated by ;")
+                } yield acc
+            }
+    } yield acc
 
   def main(args: Array[String]): Unit = {
     IronWorkManBot.run()
